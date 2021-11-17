0260538efb98898bd8cb6ae7c292d666070a1c84
Maertens Yann Christophe <iroshinidako@gmail.com>
2021-11-17 09:30:29 +0100

    Initial Commit

---
+++ StoryTiling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StoryTiling
+//
+//  Created by Maertens Yann-Christophe on 17/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StoryTiling/StoryTilingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryTilingApp.swift
+//  StoryTiling
+//
+//  Created by Maertens Yann-Christophe on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct StoryTilingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
